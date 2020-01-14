8a127d2a00ade18d6f4a39cf1ff06fa4341a6b12
dburgess <dpburgess88@gmail.com>
2020-01-13 23:56:23 -0500

    modal buttons working
    Got the modal prev and next buttons working.  The search functionality works, and the background color was changed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,34 @@
 window.onload = function() {
   base_url = "https://randomuser.me/api/?results=12&nat=US";
+  let bodyElem = document.getElementsByTagName("body")[0];
   let gallery = document.getElementById("gallery");
   let cards;
   let employees;
+  let currentId;
+
   loadSearch();
 
   fetch(base_url)
     .then(checkStatus)
     .then(response => response.json())
-    .then(data => createCard(data))
+    .then(createCard)
     .then(() => {
-      const submit = document.getElementById("search-submit");
-      submit.addEventListener("click", e => search(e));
-
+      // for each card add an event listener to create a modal when a card is clicked
       cards = document.querySelectorAll(".card");
-      //console.log(cards);
+
       cards.forEach(card => {
-        card.addEventListener("click", e => modalUse(e));
+        card.addEventListener("click", e => {
+          modalUse(e);
+        });
       });
+    })
+    .catch(e => {
+      console.log("Something happened");
     });
 
+  // -----------------
+  // HELPER FUNCTIONS
+  // -----------------
   function checkStatus(response) {
     if (response.ok) {
       return Promise.resolve(response);
@@ -28,11 +37,7 @@ window.onload = function() {
     }
   }
 
-  // -----------------
-  // HELPER FUNCTIONS
-  // -----------------
   function createCard(persons) {
-    //console.log(persons);
     employees = persons.results;
     console.log(employees);
     persons.results.map((person, index) => {
@@ -54,45 +59,25 @@ window.onload = function() {
   }
 
   function modalUse(e) {
-    let bodyElem = document.getElementsByTagName("body")[0];
-    let currentId = e.currentTarget.id;
+    //let bodyElem = document.getElementsByTagName("body")[0];
+    currentId = parseInt(e.currentTarget.id);
 
-    // console.log(e.currentTarget.innerText);
     if ([...e.currentTarget.classList].includes("card")) {
-      //console.log(e.currentTarget.id);
-      let modalEmp = employees[e.currentTarget.id];
+      let modalEmp = employees[currentId];
       let modal = createModal(modalEmp);
 
       bodyElem.appendChild(modal);
       modal.style.display = "block";
 
-      let closeModal = document.getElementById("modal-close-btn");
-      closeModal.addEventListener("click", () => {
-        bodyElem.removeChild(modal);
-      });
-
-      // end of functionality for modal appearing
-
-      let prevButton = document.getElementById("modal-prev");
-      prevButton.addEventListener("click", e => {
-        bodyElem.removeChild(modal);
-
-        if (currentId > 0) {
-          let prevEmp = employees[currentId - 1];
-          let prevModal = createModal(prevEmp);
-          bodyElem.appendChild(prevModal);
-          prevModal.style.display = "block";
-
-          let closeModal = document.getElementById("modal-close-btn");
-          closeModal.addEventListener("click", () => {
-            bodyElem.removeChild(prevModal);
-          });
-          currentId -= 1;
-        } else {
-          // disable the prev button
-        }
-      });
+      closeModal(modal);
     }
+  }
+
+  function closeModal(modal) {
+    let closeModal = document.getElementById("modal-close-btn");
+    closeModal.addEventListener("click", () => {
+      bodyElem.removeChild(modal);
+    });
   }
 
   function createModal(modalEmp) {
@@ -127,62 +112,10 @@ window.onload = function() {
             </div>
           </div> 
         `;
-
     return modal;
   }
 
-  function previous(id) {
-    console.log(id);
-    if (id > 0) {
-      let empNum = id - 1;
-      let modalEmp = employees[empNum];
-      let modal = document.createElement("div");
-      modal.classList.add("modal-container");
-      modal.innerHTML = `
-          <div class="modal">
-              <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-              <div class="modal-info-container">
-                  <img class="modal-img" src="${
-                    modalEmp.picture.large
-                  }" alt="profile picture">
-                  <h3 id="name" class="modal-name cap">${modalEmp.name.first} ${
-        modalEmp.name.last
-      }</h3>
-                  <p class="modal-text">${modalEmp.email}</p>
-                  <p class="modal-text cap">${modalEmp.location.city}</p>
-                  <hr>
-                  <p class="modal-text">${modalEmp.cell}</p>
-                  <p class="modal-text">${modalEmp.location.street.number} ${
-        modalEmp.location.street.name
-      }, ${modalEmp.location.city}, ${modalEmp.location.state} ${
-        modalEmp.location.postcode
-      }</p>
-                  <p class="modal-text">Birthday: ${parseBirthday(
-                    modalEmp.dob.date
-                  )}</p>
-              </div>
-            <div class="modal-btn-container">
-              <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-              <button type="button" id="modal-next" class="modal-next btn">Next</button>
-            </div>
-          </div> 
-        `;
-
-      let bodyElem = document.getElementsByTagName("body")[0];
-      bodyElem.appendChild(modal);
-      modal.style.display = "block";
-
-      let closeModal = document.getElementById("modal-close-btn");
-      closeModal.addEventListener("click", () => {
-        bodyElem.removeChild(modal);
-      });
-    }
-  }
-
-  function next() {}
-
   function parseBirthday(dateString) {
-    let shortDate = dateString.substring(0, 10);
     return dateString
       .substring(0, 10)
       .replace(/(\d{4})-(\d{2})-(\d{2})/, "$2 / $3 / $1");
@@ -199,16 +132,15 @@ window.onload = function() {
   }
 
   function search(e) {
+    e.preventDefault();
     let input = document.getElementById("search-input");
     let searchTerm = input.value.toLowerCase();
-    console.log(searchTerm);
-    console.log(employees);
+
     const results = employees.filter(
       emp =>
         !emp.name.first.toLowerCase().includes(searchTerm) &&
         !emp.name.last.toLowerCase().includes(searchTerm)
     );
-    console.log(results);
 
     // loops through each employee that did not match the search criteria and will hide them
     for (let i = 0; i < cards.length; i++) {
@@ -219,6 +151,50 @@ window.onload = function() {
       }
     }
   }
+
+  // -----------------
+  // EVENT LISTENERS
+  // -----------------
+
+  // adding the search functionality to the search element
+  const submit = document.getElementById("search-submit");
+  submit.addEventListener("click", e => search(e));
+
+  // event handler for the prev and next buttons on the modal
+  document.addEventListener("click", e => {
+    let element = e.target;
+    if (element.classList.contains("modal-prev")) {
+      // remove the modal
+
+      if (currentId > 0) {
+        let oldModal = document.querySelector(".modal-container");
+        bodyElem.removeChild(oldModal);
+        let prevEmp = currentId - 1;
+        let modalEmp = employees[prevEmp];
+        let newModal = createModal(modalEmp);
+        bodyElem.appendChild(newModal);
+        newModal.style.display = "block";
+        closeModal(newModal);
+        currentId -= 1;
+      } else {
+        e.target.style.display = "none";
+      }
+    } else if (element.classList.contains("modal-next")) {
+      if (currentId < 11) {
+        let oldModal = document.querySelector(".modal-container");
+        bodyElem.removeChild(oldModal);
+        let nextEmp = currentId + 1;
+        let nextModalEmp = employees[nextEmp];
+        let nextModal = createModal(nextModalEmp);
+        bodyElem.appendChild(nextModal);
+        nextModal.style.display = "block";
+        closeModal(nextModal);
+        currentId += 1;
+      } else {
+        e.target.style.display = "none";
+      }
+    }
+  });
 
   // end of file
 };
